51116211eba555da7e067304fd2c80585b4c6126
Sean B <butts.sean01@gmail.com>
2020-08-09 21:06:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,34 @@
 class TicTacToe
   
-  attr_accessor 
+  def initialize
+    @board = [ " ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
   
-  @@new = []
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+    [0,4,8],
+
+    ]
   
-  def initialize(board)
-    
-  
+def display_board
+  puts "#{@board[0]} |#{@board[1]} | #{@board[2]}"
+  puts "---------------"
+  puts "#{@board[3]} |#{@board[4]} | #{@board[5]}"
+  puts "---------------"
+  puts "#{@board[6]} |#{@board[7]} | #{@board[8]}"
+end
+
+def input_to_index(input)
+  input.to_i - 1 
+end
+
   
 end
 
